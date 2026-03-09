dc3dcd86a440b17457b15868115de1ae78293878
adavoli91 <alessandro.davoli91@gmail.com>
2026-03-09 10:35:37 +0100

    Bug fix

--- dashboard.py
+++ dashboard.py
@@ -271,7 +271,7 @@ class DashboardPAC:
 if __name__ == '__main__':
     st.set_page_config(layout = 'wide')
     #
-    analisi = st.radio(label = 'Tipo di analisi', options = ['PAC', 'Lazy portfolio'])
+    analisi = st.radio(label = 'Tipo di analisi', options = [None, 'PAC', 'Lazy portfolio'])
     #
     if analisi == 'PAC':
         DashboardPAC().main()
